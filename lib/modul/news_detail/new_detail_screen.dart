ad51631517af68799a7e085bf89e8a35658ebc63
Fadiasitinursifa <nursifafadia2@gmail.com>
2023-03-30 13:53:58 +0700

    detail anime

--- lib/modul/news_detail/new_detail_screen.dart
+++ lib/modul/news_detail/new_detail_screen.dart
@@ -1,46 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:pertemuan_v/models/news.dart';
 
-class NewsDetailScreen extends StatelessWidget {
-  NewsDetailScreen({super.key, required this.news});
-  News news;
-  int rating = 5;
+class NewsDetailScreen extends StatefulWidget {
+  final News news;
+
+  NewsDetailScreen({Key? key, required this.news}) : super(key: key);
+
+  @override
+  _NewsDetailScreenState createState() => _NewsDetailScreenState();
+}
+
+class _NewsDetailScreenState extends State<NewsDetailScreen> {
+  int _rating = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Anime Detail", style: TextStyle(fontWeight: FontWeight.bold),),
+        title: const Text("News Detail"),
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: ListView(children: [
-          Image.network(news.image!),
-          const SizedBox(height: 10),
-          Text(
-            news.title!,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 5),
-          Row(
-            children: [
-              Icon(Icons.star, color: Colors.yellow),
-              const SizedBox(width: 5),
-              Text(
-                'Rating $rating / 5',
-                style: TextStyle(fontSize: 16),
+        child: ListView(
+          children: [
+            Image.network(widget.news.image!),
+            const SizedBox(height: 1),
+            Text(
+              widget.news.title!,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Text(
-            news.description!,
-            textAlign: TextAlign.justify,
-          ),
-        ]),
+            ),
+            const SizedBox(height: 1),
+            Row(
+              children: [
+                InkWell(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.yellow,
+                    size: 24.0,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _rating = (_rating + 1) % 11;
+                    });
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Rating increased to $_rating')),
+                    );
+                  },
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  'Rating $_rating / 10',
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(height: 1),
+            Text(
+              widget.news.description!,
+              textAlign: TextAlign.justify,
+            ),
+          ],
+        ),
       ),
     );
   }
