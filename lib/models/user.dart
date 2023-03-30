45b37ec90e57c4fb7a6b141cd36d0354238a7d55
shopie14 <sh.pinh14@gmail.com>
2023-03-30 11:39:28 +0700

    praktikum mobile P05

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -17,12 +17,13 @@ class User {
 
   factory User.dummy() {
     return User(
-      id: 014,
-      name: "Shopi Nurhidayanti",
-      userName: "shoyaaa.is",
-      email: "2006061@itg.ac.id",
-      profileImage: "",
-      phoneNumber: "081511548595",
+      id: 20,
+      name: "Fadia Siti Nursifa",
+      userName: "sifa.dia",
+      email: "2006076@itg.ac.id",
+      profileImage:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgWmbJ3C3OaZ5R5_k5gedfywbLbuZbgsHiQuBIr2S6wCMF9Vri_DgXwIp3Cp3I4j_NQQg&usqp=CAU",
+      phoneNumber: "089635729696",
     );
   }
 }
